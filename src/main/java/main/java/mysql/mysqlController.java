d6311f8d3276446aa5ef0f831ae98442736af1f5
KamLan <adouardcamille@hotmail.com>
2018-07-12 12:40:55 +0200

    select and update working

--- src/main/java/main/java/mysql/mysqlController.java
+++ src/main/java/main/java/mysql/mysqlController.java
@@ -1,6 +1,5 @@
 package main.java.mysql;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import net.minidev.json.JSONArray;
@@ -15,18 +14,24 @@ import static java.lang.System.out;
 public class mysqlController {
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @RequestMapping(value = "/connect/{user}/{password}/{server}/{request}", method = RequestMethod.POST)
+    @RequestMapping(value = "/connect/{user}/{password}/{server}/{type}/{request}", method = RequestMethod.POST)
     public JSONArray mysqlUser(@PathVariable(value="user") String user,
                                 @PathVariable(value="password") String password,
                                 @PathVariable(value="server") String server,
+                                @PathVariable(value="type") String type,
                                 @PathVariable(value="request") String request){
         out.print("Post triggered");
         request = request.replace("%20", " ");
-        JSONArray JsonArray = mysqlConnector(user, password, server, request);
-        return JsonArray;
+        JSONArray JsonArray = mysqlConnector(user, password, server, type, request);
+        if (type.equals("UPDATE")){
+            return JsonArray;
+        }
+        else{
+            return JsonArray;
+        }
     }
 
-    @RequestMapping(value = "/connect/{user}/{password}/{server}", method = RequestMethod.GET)
+    @RequestMapping(value = "/connect/{user}/{password}/{server}/{request}", method = RequestMethod.GET)
     public mysql mysql(@PathVariable(value="user") String user,
                            @PathVariable(value="password") String password,
                            @PathVariable(value="server") String server,
@@ -37,7 +42,7 @@ public class mysqlController {
     @RequestMapping(value = "/request", method = RequestMethod.GET)
     public JSONArray mysqlDisplayJSONArray(){
         String request= "select * from employees order by emp_no asc limit 10";
-        JSONArray JsonArray = mysqlConnector("root", "secret", "172.17.0.2", request);
+        JSONArray JsonArray = mysqlConnector("root", "secret", "172.17.0.2", "select", request);
         return JsonArray;
     }
 
@@ -80,7 +85,7 @@ public class mysqlController {
         }
     }
 
-    public JSONArray mysqlConnector(String user, String password, String server, String request){
+    public JSONArray mysqlConnector(String user, String password, String server, String type, String request){
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setUser(user);
         dataSource.setPassword(password);
@@ -88,16 +93,21 @@ public class mysqlController {
         JSONArray JsonArray = null;
         try {
             Statement st;
-            ResultSet rs;
+            ResultSet rs = null;
+            PreparedStatement ps;
             Connection connection = (Connection) dataSource.getConnection();
             st = connection.createStatement();
-            //st.executeUpdate("CREATE DATABASE coucou;");
             st.executeQuery("USE employees;");
-            //rs = st.executeQuery("select * from employees order by emp_no asc limit 10;");
-            rs = st.executeQuery(request);
-            //displaySQLrequest(rs);
-            //out.print(convertToJSON(rs));
-            JsonArray = convertToJSON(rs);
+            if (type.equals("UPDATE")){
+                out.print(type);
+                ps = connection.prepareStatement(request);
+                ps.executeUpdate();
+            }
+            else{
+                out.print(type);
+                rs = st.executeQuery(request);
+                JsonArray = convertToJSON(rs);
+            }
             out.print("connected");
             connection.close();
 
